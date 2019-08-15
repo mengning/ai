9767689dfa76dc2fd1ef758b1c6ea37221a92207
mengning <mengning@ustc.edu.cn>
2019-08-15 15:52:25 +0800

    add readme in lab6

--- lab6/README.md
+++ lab6/README.md
@@ -6,6 +6,7 @@ pip install cmake
 pip install face_recognition
 pip install opencv-python
 git clone https://github.com/mengning/ai.git
-cd ai/lab5
-ai/lab5$ python face_locate_video_example.py
+cd ai/lab6
+# 向known_images目录增加本人头像图片，注意图片格式jpg和像素大小500-600左右
+ai/lab6$ python face_recognition_video_example.py
 ```
--- lab6/face_recognition_video_example.py
+++ lab6/face_recognition_video_example.py
@@ -41,7 +41,7 @@ while True:
         face_names = []
         for face_encoding in face_encodings:
             # See if the face is a match for the known face(s)
-            matches = face_recognition.compare_faces(known_face_encodings, face_encoding,tolerance=0.35)
+            matches = face_recognition.compare_faces(known_face_encodings, face_encoding, tolerance=0.35)
             name = "Unknown"
             # # If a match was found in known_face_encodings, just use the first one.
             if True in matches:
